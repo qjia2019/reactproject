489a7b619ded0c4cbffb049228a1dcb85316fb80
qjia2019 <qjia2019@gmail.com>
2021-08-19 20:35:46 -0400

    first commit

---
+++ todo/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Adam",
+      todoItems: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: false },
+        { action: "Call Joe", done: false },
+      ],
+      newItemText: "",
+    };
+  }
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  };
+  createNewTodo = () => {
+    if (
+      !this.state.todoitems.find(
+        (item) => item.action === this.state.newItemText
+      )
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          { action: this.state.newItemText, done: false },
+        ],
+        newItemText: "",
+      });
+    }
+  };
+  render = () => (
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        {this.state.userName}'s To Do List (
+        {this.state.todoItems.filter((t) => !t.done).length} items to do)
+      </h4>
+      <div className="container-fluid">
+        <div className="my-1">
+          <input
+            className="form-control"
+            value={this.state.newItemText}
+            onChange={this.updateNewTextValue}
+          />
+          <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
